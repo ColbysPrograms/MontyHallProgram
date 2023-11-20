0869dd421a19372412c62bc677160372445afd83
Colby Cox <wcox@cub.uca.edu>
2023-11-20 00:24:25 -0600

    Update montyHallProgram.py

--- montyHallProgram.py
+++ montyHallProgram.py
@@ -1,6 +1,7 @@
 import math
 import random
 import time
+import matplotlib.pyplot as plt
 
 def montyHall(total, pick, win, open, switch):
 
@@ -39,7 +40,7 @@ def montyHall(total, pick, win, open, switch):
 
     #switches if true
     if switch:
-        for i in range(total):
+        for i in range(total + 1):
             if i in picked:
                 picked.remove(i)
             elif i not in opened:
@@ -49,29 +50,48 @@ def montyHall(total, pick, win, open, switch):
     #decides winner or loser
     picked = set(picked)
     winners = set(winners)
-    if picked.intersection(winners):
+    if len(picked.intersection(winners)) > 0:
         return 1
     else:
         return 0
 
 def main():
     start = time.time()
-    total = 80
+    total = 3
     pick = 1
-    win = 5
-    open = 9
-    runs = 100000
-    totalWinsNoSwitch = 0
-    totalWinsSwitch = 0
+    win = 1
+    open = 1
+    runs = 1000
+    simulationNoSwitch = 0
+    simulationSwitch = 0
     switch = False
-    for i in range(runs):
-        totalWinsNoSwitch += montyHall(total, pick, win, open, False)
-        totalWinsSwitch += montyHall(total, pick, win, open, True)
-    resultNoSwitch = totalWinsNoSwitch / runs
-    resultSwitch = totalWinsSwitch / runs
-    print("Result without switching: " + str(resultNoSwitch))
-    print("Result with switching: " + str(resultSwitch))
+    probabilitiesNoSwitch = []
+    probabilitiesSwitch = []
+    for i in range(1, runs + 1):
+        simulationNoSwitch += montyHall(total, pick, win, open, False)
+        simulationSwitch += montyHall(total, pick, win, open, True)
+        probabilitiesNoSwitch.append(simulationNoSwitch / i)
+        probabilitiesSwitch.append(simulationSwitch / i)
+    simulationNoSwitch = simulationNoSwitch / runs
+    simulationSwitch = simulationSwitch / runs
+    print("Simulation's probability with no switching: " + str(simulationNoSwitch))
+    print("Simulation's probability with switching: " + str(simulationSwitch))
+    trueNoSwitch = 1 - (math.factorial(total - win) * math.factorial(total - pick)) / (math.factorial(total) * math.factorial(total - win - pick))
+    trueSwitch = 0
+    print("True probability with no switching: " + str(trueNoSwitch))
+    print("True probability with switching: " + str(trueSwitch))
     end = time.time()
-    print(end - start)
+    graph(probabilitiesNoSwitch)
+    graph(probabilitiesSwitch)
+    print("Time taken: " + str(end - start))
+
+def graph(probabilities):
+    #takes probabilities vector and makes a graph of how probability changes over the simulations
+    plt.plot(probabilities)
+    plt.ylabel('Probability')
+    plt.xlabel('Simulations')
+    ax = plt.gca()
+    ax.set_ylim([0, 1])
+    plt.show()
 
 main()
